3b5b7913966462ce6bc503d129619175145c2f4e
sarah wang <sarahwang47@gmail.com>
2020-09-07 15:41:55 +0800

    done user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,29 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = 'user info has been updated'
+      redirect_to root_path
+    else
+      flash[:alert] = 'something went wrong'
+      render action:edit
+    end
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:image)
   end
 
 end
