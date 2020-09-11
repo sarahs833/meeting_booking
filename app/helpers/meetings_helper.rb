bd842682135ea2bbd6b2696088d643440d222895
sarah wang <sarahwang47@gmail.com>
2020-09-11 17:27:16 +0800

    meeting participants or creator

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,9 +1,19 @@
 module MeetingsHelper
   def meeting_participants(meeting)
     if meeting.users.present?
-      "participants: #{meeting.users.pluck(:username).join(",")}"
+      "#{pluralize(meeting.users.count,'participant')} : #{meeting.users.pluck(:username).join(",")}"
     else
       "They are no participants yet"
+    end
+  end
+
+  def meeting_tag(meeting)
+    if meeting.creator == current_user
+      "you have created this meeting"
+    elsif meeting.users.include?(current_user)
+      "you have been tag to participate to the meeting"
+    else
+      ""
     end
   end
 end
